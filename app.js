fcb7091d2897eea03e1fee58568b41ccae48bc89
mashuco <mashuco@nifty.com>
2026-06-27 00:51:25 +0900

    Deploy FS data from scenario YAML

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { firebaseConfig } from "./firebase-config.js";
-import { fsData } from "./fs-data.js?v=fs-5f196bc2b83d";
+import { fsData } from "./fs-data.js?v=fs-a9392d3fabd6";
 import { happeningChart } from "./happening-data.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import {
